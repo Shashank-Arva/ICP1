93dd33e089a208c785a5a23f5a5ea640a2c43622
Shashank-Arva <156723989+Shashank-Arva@users.noreply.github.com>
2024-01-16 13:21:48 -0600

    Add files via upload

---
+++ Second question.py
@@ -0,0 +1,10 @@
+# Write a program that accepts a sentence and replace each occurrence of ‘python’ with ‘pythons’.
+
+# Receive a sentence input from the user
+sentence = input("Enter a sentence: ")
+
+# Substitute every occurrence of 'python' with 'pythons'
+modified_sentence = sentence.replace('python', 'pythons')
+
+# Printing the modified sentence
+print("Modified Sentence:", modified_sentence)
